08205931511a53311bce55e9c6abbcfecd6db289
Alice Chuang <bomberetta@gmail.com>
2018-04-03 12:15:16 -0400

    rename

---
+++ 3rd-Edition-2018/0.0_solutions.js
@@ -0,0 +1,2 @@
+// Eloquent JS 3rd Edition 2018
+// Find Official Solutions Here - https://eloquentjavascript.net/code/
--- 3rd-Edition-2018/1.0_solutions.js
+++
@@ -1,2 +0,0 @@
-// Eloquent JS 3rd Edition 2018
-// Find Solutions Here - https://eloquentjavascript.net/code/
